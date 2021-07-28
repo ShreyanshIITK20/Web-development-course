8f9be022aa4b8a71e1c3bfc44c4d4e577d430a20
Shreyansh Agarwal <shreyansha20@iitk.ac.in>
2021-07-28 19:44:09 +0530

    Adding audio in JS

--- Drum Kit Starting Files/Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/Drum Kit Starting Files/index.js
@@ -3,6 +3,15 @@ for(var i=0;i<numberOfDrums;i++){
     document.querySelectorAll(".drum")[i].addEventListener("click",handleClick);
 }
 
+// ABOVE TASK USING AN ANONYMOUS FUNCTION IN SECOND ARGUMENT OF addEventListener
+// for(var i=0;i<numberOfDrums;i++){
+//     document.querySelectorAll(".drum")[i].addEventListener("click",function () {
+//         alert("I got clicked!");
+//     });
+// }   
+
 function handleClick() {
-    alert("I got clicked!");
+    var audio = new Audio("sounds/tom-1.mp3");
+    audio.play();
 }
+
