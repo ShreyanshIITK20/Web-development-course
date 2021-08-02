a833cb10b7ee51444f21b6eb19f275c5d9e3da32
Shreyansh Agarwal <shreyansha20@iitk.ac.in>
2021-08-02 16:19:22 +0530

    Completed Simon game

--- Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/game.js
+++ Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/game.js
@@ -1,26 +1,74 @@
 var userClickedPattern = [];
 var gamePattern = [];
 var buttonColours = ["red","blue","green","yellow"];
+var level = 0;
+var started = false;
+
+$(document).keypress(function(){                //keypress to start the game
+    if(started==false){
+        $("h1").text("Level "+level);
+        nextSequence();
+        started = true;
+    }
+});
+
+//jquery code to run when we click any button
+$(".btn").click(function(){
+    var userChosenColour = $(this).attr('id');                  //storing ID of clicked button
+    userClickedPattern.push(userChosenColour);                  //remembering the pattern 
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+    checkAnswer(userClickedPattern.length-1)
+})
+
 function nextSequence(){
+    level++;
+    $("h1").text("Level "+level);
+
+    userClickedPattern = []                                                        //re-setting the user input pattern
+    
     var randomNumber = Math.floor((Math.random())*4);
-    
     var randomChoosenColour = buttonColours[randomNumber];
-    
     gamePattern.push(randomChoosenColour);
     
     $("#"+randomChoosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);  //flash animation on button selected
-    
-    playSound(randomChoosenColour);
+    playSound(randomChoosenColour);                                                //sound play for button selected
 }
 
-$(".btn").click(function(){
-    var userChosenColour = $(this).attr('id');                  //storing ID of clicked button
-    userClickedPattern.push(userChosenColour);
-    playSound(userChosenColour);
-
-})
-
+//function to play sound on clicking
 function playSound(name){
     var buttonAudio = new Audio("sounds/"+name+".mp3");
     buttonAudio.play();
+}
+
+//function to flash the button on clicking
+function animatePress(currentColour){
+    $("#"+currentColour).addClass("pressed");
+    setTimeout(function(){
+        $("#"+currentColour).removeClass("pressed");
+    },100);
+}
+
+function checkAnswer(currentLevel){
+    if(userClickedPattern[currentLevel]===gamePattern[currentLevel]){
+        if(userClickedPattern.length===gamePattern.length){
+            setTimeout(function(){
+                nextSequence();
+            },1000);
+        }
+    } else{
+        playSound("wrong");
+        $("body").addClass("game-over");
+        setTimeout(function(){
+            $("body").removeClass("game-over");
+        },200);
+        $("h1").text("Game Over, Press Any Key to Restart");
+        startover();
+    }
+}
+
+function startover(){
+    started = false;
+    level = 0;
+    gamePattern = [];
 }
