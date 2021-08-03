6a054739ab6874031d1c374cfe3ee3b6277b49a7
Shreyansh Agarwal <shreyansha20@iitk.ac.in>
2021-08-03 20:37:10 +0530

    Successfully set up a working calculator

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,7 +1,16 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
 app.listen(3000);
 
+app.post("/",function(req,res){
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+    var result = num1 + num2;
+    res.send("Calculated sum of given numbers is "+result);
+});
 app.get("/",function(req,res){
-    res.send("Hello, this is a calculator");
+    res.sendFile(__dirname+"/index.html")
 });
