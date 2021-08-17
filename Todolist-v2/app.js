1ba57dcd367e83a20ed01b1e73e74f6854b3dacd
Shreyansh Agarwal <shreyansha20@iitk.ac.in>
2021-08-17 20:31:44 +0530

    Made some minor changes

--- Todolist-v2/app.js
+++ Todolist-v2/app.js
@@ -13,7 +13,7 @@ mongoose.set('useFindAndModify', false);
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB",{useNewUrlParser:true,useUnifiedTopology: true});    //connecting mongoose to port 27017
+mongoose.connect("mongodb+srv://admin-shreyansh:rangeroverlove@cluster0.cihvc.mongodb.net/todolistDB",{useNewUrlParser:true,useUnifiedTopology: true});    //connecting mongoose to port 27017
 
 const itemsSchema = {                                                               //format or schema of the list we are creating
   name : String
@@ -136,6 +136,11 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+
+app.listen(port, function() {
+  console.log("Server started successfully");
 });
