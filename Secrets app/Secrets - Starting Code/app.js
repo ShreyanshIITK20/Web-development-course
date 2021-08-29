f55995e9b905244ffb1e5c6a5c5c1e4dfed1d322
Shreyansh Agarwal <shreyansha20@iitk.ac.in>
2021-08-29 17:57:47 +0530

    bcrypt hash + salting (Level 4)

--- Secrets app/Secrets - Starting Code/app.js
+++ Secrets app/Secrets - Starting Code/app.js
@@ -3,7 +3,9 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const express = require("express");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;                                                          //successive rounds of salting done during the procedure to get the final password
+// const md5 = require("md5");
 // const encrypt = require("mongoose-encryption");
 
 const app = express();
@@ -40,14 +42,17 @@ app.get("/register",function(req,res){
 
 //user will go to register page and enter an email and password which will be submitted as a POST request which we are required to catch now
 app.post("/register",function(req,res){
-    const newUser = new User({                          //created a new user's account using the information posted through register form
-        email: req.body.username,
-        password: md5(req.body.password)
-    });
     
-    newUser.save(function(err){
-        if(err) console.log(err);
-        else res.render("secrets");                     //if there are no errors then render the secrets.ejs page and it is the only way to access the secrets page since above we have no other GET request for secrets page
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser = new User({                          //created a new user's account using the information posted through register form
+            email: req.body.username,
+            password: hash                                  //hashed password created using the bcrypt
+        });
+        
+        newUser.save(function(err){
+            if(err) console.log(err);
+            else res.render("secrets");                     //if there are no errors then render the secrets.ejs page and it is the only way to access the secrets page since above we have no other GET request for secrets page
+        }); 
     });
 });
 
@@ -55,15 +60,16 @@ app.post("/register",function(req,res){
 //For the login page user will come and enter his email and password and we will have to check whether an account with those credentials exist or not in our database
 app.post("/login",function(req,res){
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
     User.findOne({email:username},function(err,foundUser){
         if(err) console.log(err);
         else {
             if(foundUser){                                          //foundUser = 0 if nobody found so it wont evaluate
-                if(foundUser.password === password){                //foundUser's password is same as entered password in our for
-                    res.render("secrets");
-                }                 
+                bcrypt.compare(password, foundUser.password, function(err, result) {            //bcrypt's compare method to compare the user entered password and the hashed password
+                    console.log(result);
+                    if(result===true) res.render("secrets");                                    //if found a user with same hash then result will be true else false
+                });               
             }
         }
     });
